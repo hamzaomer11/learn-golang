1c05c37ce1ca8c59d387e1fd2ba452ab8217347c
hamesomer11 <hamzaomer45@gmail.com>
2025-07-16 23:29:40 +0100

    Array_and_Slices - Commit #5

--- Array-and-Slices/sum_test.go
+++ Array-and-Slices/sum_test.go
@@ -20,13 +20,11 @@ func TestSum(t *testing.T) {
 
 }
 
-func TestSumAll(t *testing.T) {
-
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	expected := []int{3, 9}
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	expected := []int{2, 9}
 
 	if !reflect.DeepEqual(got, expected) {
-		t.Errorf("got %v want %v", got, expected)
+		t.Errorf("got %v expectec %v", got, expected)
 	}
-
 }
