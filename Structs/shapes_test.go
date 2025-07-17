4c771ff061aacf78955961f42396cf2b370411f2
hamesomer11 <hamzaomer45@gmail.com>
2025-07-17 06:59:50 +0100

    Shapes - Final Commit #2

--- Structs/shapes_test.go
+++ Structs/shapes_test.go
@@ -17,20 +17,23 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTests := []struct {
-		shape    Shape
-		expected float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, expected: 72.0},
-		{shape: Circle{Radius: 10}, expected: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, expected: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
-		if got != tt.expected {
-			t.Errorf("got %g expected %g", got, tt.expected)
-		}
+			if got != tt.hasArea {
+				t.Errorf("got %g expected %g", got, tt.hasArea)
+			}
+		})
 	}
 
 }
