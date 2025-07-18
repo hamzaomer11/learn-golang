6fd832a1d233669243f59262ba848250f46fef8a
hamesomer11 <hamzaomer45@gmail.com>
2025-07-18 23:24:42 +0100

    Pointers - Commit #1

---
+++ Pointers/wallet_test.go
@@ -0,0 +1,18 @@
+package pointers
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	expected := 10
+
+	if got != expected {
+		t.Errorf("got %d expected %d", got, expected)
+	}
+
+}
