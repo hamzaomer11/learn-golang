92a4f56aa7932e62b9918d71a292b61859a8e52b
hamesomer11 <hamzaomer45@gmail.com>
2025-07-19 20:56:31 +0100

    Maps - Commit #2

--- Dictionary/dictionary.go
+++ Dictionary/dictionary.go
@@ -1,5 +1,21 @@
 package dictionary
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word i'm looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
--- Dictionary/dictionary_test.go
+++ Dictionary/dictionary_test.go
@@ -1,17 +1,55 @@
 package dictionary
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	t.Run("known word", func(t *testing.T) {
+
+		got, _ := dictionary.Search("test")
+		expected := "this is just a test"
+
+		assertStrings(t, got, expected)
+
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+
+		_, got := dictionary.Search("unknown")
+		if got == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
 	expected := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word: ", err)
+	}
 
 	assertStrings(t, got, expected)
 }
 
 func assertStrings(t testing.TB, got, expected string) {
+	t.Helper()
+	if got != expected {
+		t.Errorf("got %q expected %q", got, expected)
+	}
+}
+
+func assertError(t testing.TB, got, expected error) {
 	t.Helper()
 	if got != expected {
 		t.Errorf("got %q expected %q", got, expected)
