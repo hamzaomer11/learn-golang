c5a707da8d524e8d2b1c96fea92572a92dc43d5d
hamesomer11 <hamzaomer45@gmail.com>
2025-07-20 17:30:48 +0100

    Mocking - Commit #3

--- Mocking/countdown_test.go
+++ Mocking/countdown_test.go
@@ -7,7 +7,8 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
-	Countdown(buffer)
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	expected := `3
@@ -17,6 +18,10 @@ Go!`
 
 	if got != expected {
 		t.Errorf("got %q expected %q", got, expected)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 
 }
