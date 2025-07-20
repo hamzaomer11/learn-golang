0d5d13277f84474aa6f787be074e478e98d5fbb6
hamesomer11 <hamzaomer45@gmail.com>
2025-07-20 16:41:21 +0100

    Mocking - Commit #1

---
+++ Mocking/countdown_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	Countdown(buffer)
+
+	got := buffer.String()
+	expected := "3"
+
+	if got != expected {
+		t.Errorf("got %q expected %q", got, expected)
+	}
+
+}
