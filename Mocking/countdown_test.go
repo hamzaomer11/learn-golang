666fdc1cc42b1da2567eb277230659ff33109c1a
hamesomer11 <hamzaomer45@gmail.com>
2025-07-20 18:11:01 +0100

    Mocking - Commit #4

--- Mocking/countdown_test.go
+++ Mocking/countdown_test.go
@@ -2,26 +2,61 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
-	Countdown(buffer, spySleeper)
+	t.Run("prints 3 to go", func(t *testing.T) {
 
-	got := buffer.String()
-	expected := `3
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		expected := `3
 2
 1
 Go!`
 
-	if got != expected {
-		t.Errorf("got %q expected %q", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %q expected %q", got, expected)
+		}
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("expected calls %v got %v", expected, spySleepPrinter.Calls)
+		}
+
+	})
+
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.SetDurationSlept}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
 
 }
