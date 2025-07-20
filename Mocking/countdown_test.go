206ac6ad75ed55a3d13d62908f83bf0db40c75f4
hamesomer11 <hamzaomer45@gmail.com>
2025-07-20 16:57:34 +0100

    Mocking - Commit #2

--- Mocking/countdown_test.go
+++ Mocking/countdown_test.go
@@ -10,7 +10,10 @@ func TestCountdown(t *testing.T) {
 	Countdown(buffer)
 
 	got := buffer.String()
-	expected := "3"
+	expected := `3
+2
+1
+Go!`
 
 	if got != expected {
 		t.Errorf("got %q expected %q", got, expected)
