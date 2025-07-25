20757ee0317510514eeee5549b1a37b96a2a92a6
hamesomer11 <hamzaomer45@gmail.com>
2025-07-25 23:32:07 +0100

    Context - Final Commit

--- Context/context.go
+++ Context/context.go
@@ -3,6 +3,7 @@ package context
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -12,7 +13,12 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		data, _ := store.Fetch(r.Context())
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			log.Printf("error fetching from store, %v", err)
+			return
+		}
 		fmt.Fprint(w, data)
 	}
 }
