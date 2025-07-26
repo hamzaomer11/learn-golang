ea9fca7fbaec74eefd23555ca39dabcad7697ba4
hamesomer11 <hamzaomer45@gmail.com>
2025-07-26 16:55:50 +0100

    Property_Based_Testing - Commit #2

--- Property/numeral.go
+++ Property/numeral.go
@@ -1,11 +1,38 @@
 package numeral
 
+import "strings"
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 3 {
-		return "III"
+
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
 	}
-	if arabic == 2 {
-		return "II"
-	}
-	return "I"
+
+	return result.String()
 }
