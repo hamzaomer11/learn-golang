e094ce23be850fdb92e3691bf329933fe073df55
hamesomer11 <hamzaomer45@gmail.com>
2025-07-28 15:05:45 +0100

    Maths - Commit #4

--- Maths/clockface.go
+++ Maths/clockface.go
@@ -10,18 +10,6 @@ type Point struct {
 	Y float64
 }
 
-const secondHandLength = 90
-const clockCentreX = 150
-const clockCentreY = 150
-
-func SecondHand(t time.Time) Point {
-	p := SecondHandPoint(t)
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength}
-	p = Point{p.X, -p.Y}
-	p = Point{p.X + clockCentreX, p.Y + clockCentreY}
-	return p
-}
-
 func SecondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
